f65eab009e6ec24a47f85d1009bb671a5e863218
Alanna Risse <angrypirate@gmail.com>
2018-11-05 15:49:06 -0800

    adding motollla layout

--- jquery/jquery_playground/script.js
+++ jquery/jquery_playground/script.js
@@ -2,19 +2,19 @@
 //        alert( "ready!" );
 //     $('p.lipsum').fadeIn(1000); // slow, fast, miliseconds
 //    $('p.lipsum').fadeOut(1000);
-      $('p.lipsum').fadeIn(2000).fadeOut(1000);
-      $('p.slide1').slideUp(8000);
-      $('h1').html('Replaced your header, sucka');
-      $('h1').css('color', 'blue');
+      // $('p.lipsum').fadeIn(2000).fadeOut(1000);
+      // $('p.slide1').slideUp(8000);
+      // $('h1').html('Replaced your header, sucka');
+      // $('h1').css('color', 'blue');
     });
 
 
 $(function() {
     $( ".readme" ).click(function () {
       if ( $( ".slide2" ).is( ":hidden" ) ) {
-        $( ".slide2" ).slideDown( "slow" );
+        $( ".slide2" ).slideDown( 5000 );
       } else {
-        $( ".slide2" ).slideUp( "slow" );
+        $( ".slide2" ).slideUp( 3000 );
       }
     });
 
